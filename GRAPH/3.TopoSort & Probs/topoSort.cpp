4febfba61bf4deed389fc07243f904d0574a772c
Ehtasham <ehtasham8292@gmail.com>
2024-01-21 09:23:45 +0530

    PrevCommit

--- GRAPH/3.TopoSort & Probs/topoSort.cpp
+++ GRAPH/3.TopoSort & Probs/topoSort.cpp
@@ -10,3 +10,37 @@
 // }
 
 // // Call the above function and then store the values in stack in same order LIFO
+
+// -------------------------------------------------------------------------------------------------------
+
+// ------------>> KAHN'S ALGORITHM
+// vector<int> topoSort(int V, vector<int> adj[]) 
+// 	{
+// 	    // code here
+// 	    vector<int> inDegree(V,0) ;
+// 	    for(int i = 0; i < V; i++){
+// 	        for(auto it : adj[i]){
+// 	            inDegree[it]++ ;
+// 	        }
+// 	    }
+// 	    queue<int> q; 
+// 	    for(int i = 0; i < V; i++){
+// 	        if(inDegree[i] == 0) q.push(i) ;
+// 	    }
+// 	    vector<int> ans ;
+	    
+// 	    while(!q.empty()){
+// 	        int node = q.front();
+// 	        q.pop() ;
+// 	        for(auto it : adj[node]){
+// 	            if(inDegree[it] > 0) inDegree[it]-- ;
+// 	            if(inDegree[it] == 0){
+// 	                q.push(it) ;
+// 	            }
+// 	        }
+// 	        ans.push_back(node) ;
+// 	    }
+// 	    return ans ;
+// 	}
+
+// -------------------------------------------------------------------------------------------------------
